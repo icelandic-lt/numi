dea895d5887ba6cd475a47a9d63bc82de1767a9b
DavidErikMollberg <david.mollberg@tiro.is>
2022-08-02 10:28:23 +0000

    Adding functions for numbers 1.000-999.999

--- src/numi/main.py
+++ src/numi/main.py
@@ -1,14 +1,9 @@
-from curses import ALL_MOUSE_EVENTS
-from unicodedata import category
-from numi.utils import base 
-from numi.utils import declensions as dec
-import logging
+from sys import prefix
+from tkinter import BOTH
+from numi.utils import base, AT_AF, logger
+from numi.handle_input import find_valid_combinations, parse_input_string
 
-FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s()] %(message)s"
-logging.basicConfig(level=logging.DEBUG, format=FORMAT)
-logger = logging.getLogger(__name__)
-
-AT_AF = "at_af"
+logging = logger()
 
 
 def _get_from_base(n, f):
@@ -18,9 +13,13 @@ def _get_from_base(n, f):
     """
     num = base[n, f]
     if "/" in num:
-        return num.split("/")
+        num = num.split("/")
     else:
-        return [num]
+        num = [num]
+
+    logging.debug(f"Returning: {num} Input was: {n, f}")
+    return num
+
 
 def _nums_20_99(n, f):
     """
@@ -29,14 +28,25 @@ def _nums_20_99(n, f):
     n1 = int(str(n)[0] + "0")
     n2 = int(str(n)[1])
     last_number = _get_from_base(n2, f)
-    if len(last_number) == 2:
-        return [f"{base[n1,AT_AF]} og {n2}" for n2 in last_number]
-    else:
-        return [f"{base[n1,AT_AF]} og {base[n2,f]}"]
 
-def _nums_100_999(n, f):
+    if len(last_number) == 2:
+        num = [f"{base[n1,AT_AF]} og {n2}" for n2 in last_number]
+    else:
+        num = [f"{base[n1,AT_AF]} og {base[n2,f]}"]
+
+    logging.debug(f"Returning: {num} Input was: {n, f}")
+    return num
+
+
+def _nums_100_999(n, f, prefix="both"):
     """
     Returns all possible variations for the written numbers from 100 to 999 as a list of strings.
+
+    prefix values:
+        'eitt': Add the word "eitt" infront of "hundrað".
+        'hundrað': Return with only "hundrað".
+        'both': Return with both "hundrað" and "eitt hundrað".
+
     """
 
     def _hundreds(n):
@@ -46,8 +56,8 @@ def _nums_100_999(n, f):
         elif n_1 in [2, 3, 4]:
             num = f"{base[n_1,'ft_hk_nf']} hundruð"
         else:
-            num = f"{base[n_1,'at_af']} hundruð"
-        
+            num = f"{base[n_1,AT_AF]} hundruð"
+        logging.debug(f"Returning: {num} Input was: {n}")
         return num
 
     n1 = int(str(n)[0] + "00")
@@ -64,169 +74,191 @@ def _nums_100_999(n, f):
         num2 = num2
     else:
         num2 = _nums_20_99(n2, f)
-    
+
     if num2:
         num = [f"{num1} {n2}" for n2 in num2]
-    
-    for line in num:
-        if "eitt hundrað" in line:
-            num.append(line.replace("eitt hundrað", "hundrað"))
 
+    if prefix == "both":
+        for line in num:
+            if "eitt hundrað" in line:
+                num.append(line.replace("eitt hundrað", "hundrað"))
+    if prefix == "hundrað":
+        for idx, line in enumerate(num):
+            if "eitt hundrað" in line:
+                num[idx] = line.replace("eitt hundrað", "hundrað")
+    logging.debug(f"Returning: {num} Input was: {n, f}")
     return num
 
-def divert(n, f):
+
+def _nums_1000_9999(n, f, add_thousand=True):
     """
-    Handles user input and returns a list of string with all 
+    Returns all possible variations for the written numbers from 1000 to 9999 as a list of strings.
+    """
+
+    def _thousands(n):
+        n_1 = int(str(n)[0])
+        if n_1 == 1:
+            num = f"{base[n_1, 'et_hk_nf']} þúsund"
+        elif n_1 in [2, 3, 4]:
+            num = f"{base[n_1,'ft_hk_nf']} þúsund"
+        else:
+            num = f"{base[n_1,AT_AF]} þúsund"
+        logging.debug(f"Returning: {num} Input was: {n}")
+        return num
+
+    n1 = int(str(n)[0] + "000")
+    num1 = _thousands(n1)
+    n2 = n - n1
+    num2 = []
+    if n in [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]:
+        num = [_thousands(n)]
+    elif n2 < 1000 and n2 > 100:
+        num2 = _nums_100_999(n2, f, prefix="eitt")
+    elif n2 in [x for x in range(1, 20)] or n2 in [x for x in range(20, 101, 10)]:
+        for line in _get_from_base(n2, f):
+            num2.append(f"og {line}")
+    else:
+        num2 = _nums_20_99(n2, f)
+
+    if num2:
+        num = [f"{num1} {n2}" for n2 in num2]
+
+    if n == 1000 and add_thousand:
+        for line in num:
+            if "eitt þúsund" in line:
+                num.append(line.replace("eitt þúsund", "þúsund"))
+
+    logging.debug(f"Returning: {num} Input was: {n, f}")
+    return num
+
+
+def _nums_10000_99999(n, f):
+    """
+    Returns all possible variations for the written numbers from 10000 to 99999 as a list of strings.
+    """
+    num = []
+    num1 = int(str(n)[0:-3])
+    num2 = int(str(n)[-3:])
+
+    if n in [10000]:
+        return [f"{x} þúsund" for x in divert(num1, f)]
+
+    # Numbers 10000-99999
+    if num1 in [x for x in range(10, 21)] or num1 in [x for x in range(20, 101, 10)]:
+        # print(1, num1)
+        f1 = AT_AF
+    elif int(str(num1)[-1]) == 1:
+        # print(2, num1)
+        f1 = "et_hk_nf"
+
+    elif int(str(num1)[-1]) in [2, 3, 4]:
+        # print(3, num1)
+        f1 = "ft_hk_nf"
+    else:
+        # print(4, num1)
+        f1 = AT_AF
+
+    for line in divert(num2, f, prefix="eitt"):
+        # Some numbers have an "og" between num1 and num2
+        last_two = int(str(num2)[-2:])
+        if last_two in [x for x in range(0, 20)] or last_two in [
+            x for x in range(20, 91, 10)
+        ]:
+            num.append(f"{divert(num1, f1, prefix='hundrað')[0]} þúsund og {line}")
+        else:
+            num.append(f"{divert(num1, f1, prefix='hundrað')[0]} þúsund {line}")
+    logging.debug(f"Returning: {num} Input was: {n, f}")
+    return num
+
+
+def _nums_100000_999999(n, f):
+    """
+    Returns all possible variations for the written numbers from 100000 to 999999 as a list of strings.
+    """
+    num = []
+    num1 = int(str(n)[0:-3])
+    num2 = int(str(n)[-3:])
+
+    if n in [100000]:
+        return [f"{x} þúsund" for x in divert(num1, f)]
+
+    # Numbers 100000-999999
+    if int(str(num1)[-2:]) in [x for x in range(10, 20)] or int(str(num1)[-2:]) in [
+        x for x in range(20, 101, 10)
+    ]:
+        # print(1, num1)
+        f1 = AT_AF
+    elif int(str(num1)[-1]) == 1:
+        # print(2, num1)
+        f1 = "et_hk_nf"
+
+    elif int(str(num1)[-1]) in [2, 3, 4]:
+        # print(3, num1)
+        f1 = "ft_hk_nf"
+    else:
+        # print(4, num1)
+        f1 = AT_AF
+
+    for line in divert(num2, f, prefix="eitt"):
+        # Some numbers have an "og" between num1 and num2
+        # Numbers 1-9
+        if len(str(num2)) == 1:
+            logging.debug(f'1: Adding "og" {{num2}}')
+            num.append(f"{divert(num1, f1, prefix='hundrað')[0]} þúsund og {line}")
+        elif (
+            len(str(num2)) == 2
+            and num2 in [x for x in range(20, 91, 10)]
+            or num2 in range(10, 20)
+        ):
+            logging.debug(f'2: Adding "og" {num2}')
+            num.append(f"{divert(num1, f1, prefix='hundrað')[0]} þúsund og {line}")
+        else:
+            logging.debug(f'3: Not adding "og" {num2}')
+            num.append(f"{divert(num1, f1, prefix='hundrað')[0]} þúsund {line}")
+    logging.debug(f"Returning: {num} Input was: {n, f}")
+    return num
+
+
+def divert(n, f, prefix="both"):
+    """
+    Handles user input and returns a list of string with all
     possible variations of if input number
     """
-    if (
-        n in [x for x in range(1, 20)]
-        or n in [x for x in range(20, 91, 10)]
-        or n in [1000, 1000000]
-    ):
+    logging.debug(f"User input: {n,f}")
+    # Numbers 1-19 add 20, 30, 40, 50, 60, 70, 80, 90
+    if n in [x for x in range(1, 20)] or n in [x for x in range(20, 91, 10)]:
         return _get_from_base(n, f)
+    # Numbers 20 to 99
     elif n in [x for x in range(20, 100)]:
         return _nums_20_99(n, f)
-    elif n in [x for x in range(100, 1000)]:
-        return _nums_100_999(n, f)
-
-
-def parse_input_string(f):
-    """
-    This functions takes in the input string and does a few things to make the
-    package more robust and user friendly. 
-
-    Firstly, it orders the input values separeted by "_" in the correct way. This
-    means that the package is agnostic to the order the user sends in. 
-
-    Second, if the user does not specify on or more or all of the three input values
-    (number, gender, case) we handle that as if the user want all of the possible 
-    numbers, gender or cases for that input number (number here refers the lingustic number
-    i.e. singular or plural but also the input number from the user.)
-
-    Thridly, the function throws an error if the input string has e.g. two or more values
-    for one of the three possible (number, gender, case).
-    """
-
-    # Create a sorted list of all legal combination found in the base. It's sorted
-    # for the sake of testing a set is inherently unsorted
-    all_combinations = sorted(list(set([x[1] for x in base]))) 
-    correct_order = ['number',"gender", "case"]
-    # If the input string is none we return all possible combinations of
-    # the input string as a list of strings
-    if f == None:
-        return all_combinations
-
-
-    # If the input string is one of the input numbers that don't follow declention rules
-    # number we don't need look further. 
-    if f == AT_AF:
-        return [f]
-
-    len_f = len(f.split('_'))
-    # If len_f is longer than three means the user has inputed a invalid string
-    if len_f > 3:
-        raise Exception(f"Sorry, the input string is ambigous")
-
-    # Check wheter the user input e.g. the number twice
-    input_value_types = []
-    for v in f.split('_'):
-        if v not in dec:
-            raise Exception(f"Sorry, the input string is ambigous for the part {v} of the input string {f}")
-
-        if dec[v] in input_value_types:
-            raise Exception(f"Duplicate value in the input string")
-        else:
-            input_value_types.append(dec[v])
-
-    # if the length of len_f is three return the input in the correct order
-    new_f = []
-    if len_f == 3:
-        new_f.append(f)
-
-    # if the user only adds one or two values we need to find the category that is missing
-    # and populate the string with those categories. 
-    categories_present = [dec[x] for x in f.split('_')]
-    missing_categories = [x for x in correct_order if x not in categories_present]
-    
-    if len_f == 2:
-        for x in [k for k,v in dec.items() if v== missing_categories[0]]:
-            new_f.append(f"{f}_{x}")
-
-    if len_f == 1:
-        for m_one in [k for k,v in dec.items() if v== missing_categories[0]]:
-            for m_two in [k for k,v in dec.items() if v== missing_categories[1]]:
-                new_f.append(f"{f}_{m_one}_{m_two}")
-
-
-    # Now lest sort all the enties in new_f
-    for i in range(len(new_f)):
-        new_f[i] = {dec[v]:v for v in new_f[i].split('_')}
-        new_f[i] = "{}_{}_{}".format(new_f[i][correct_order[0]],
-                                 new_f[i][correct_order[1]],
-                                 new_f[i][correct_order[2]])
-    logger.debug(f"Sorted input string is {new_f[i]}")
-    return new_f
-
-
-def find_valid_combinations(n,f):
-    """
-    Given that the last number in the input number controls the declentions 
-    we check in the base wheter a combination is valid. We return all valid 
-    combinations. If there are no valid combinations an exception is raised. 
-    """
-    new_f = []
-
-
-    # if f is only one string at this point, that means the user added a
-    # three value string. Let's check if is a valid input.
-    # if len(f) == 1:
-    #     if (n,f[0]) in base:
-    #         logging.debug(f"0: Adding {n,f[0]}")
-    #         new_f.append(f[0])
-
-    if len(str(n)) > 1:
-        for v in f:
-            # if the whole number along with the input stris in the 
-            # base, add the combination. 
-            if (n,v) in base:
-                logging.debug(f"1: Adding {n,v}")
-
-                new_f.append(v)
-            # if the last two digit form a number with the input str
-            # that is the base we add that to the combination.
-            elif (int(str(n)[-2:]), v) in base:
-                logging.debug(f"2: Adding {n,v} based on {int(str(n)[-2])}") 
-
-                new_f.append(v)
-            # if the last number is in the base with the input str
-            # that is the base we add that to the combination.
-            elif (int(str(n)[-1]), v) in base:
-                logging.debug(f"3: Adding {n,v} based on {int(str(n)[-1])}")
-                new_f.append(v)
-    else:
-        if (n,f) in base:
-            logging.debug(f"4: Adding {n,f}")
-            new_f.append(f)
-
-    if new_f:
-        return new_f
-    else:
-        raise Exception(f"Sorry, can't find a valid combination with that input")
+    # Numbers from 100 to 999
+    elif len(str(n)) == 3:
+        return _nums_100_999(n, f, prefix)
+    # Numbers between 1000 and 9999
+    elif len(str(n)) == 4:
+        return _nums_1000_9999(n, f)
+    # Nmnbers between 10000 and 999999
+    elif len(str(n)) == 5:
+        return _nums_10000_99999(n, f)
+    # Nmnbers between 100000 and 9999999
+    elif len(str(n)) == 6:
+        return _nums_100000_999999(n, f)
 
 
 def spell_out(n, f=None):
     """
     Validates user input and sends the input to diverter.
-    The output is printed to the console. 
+    The output is printed to the console.
     """
-    logger.debug(f'User input: {n,f}')    
+    logging.debug(f"User input: {n,f}")
     f = parse_input_string(f)
-    logger.debug(f'The the len of f after parse input is {len(f)} and of type {type(f)}')
+    logging.debug(
+        f"The the len of f after parse input is {len(f)} and of type {type(f)}"
+    )
 
-    f = find_valid_combinations(n,f)
+    f = find_valid_combinations(n, f)
 
     if len(f) > 1:
-        return [[n, v, divert(n,v)] for v in f]
+        return [[n, v, divert(n, v)] for v in f]
     else:
-        return divert(n,f[0])
+        return divert(n, f[0])
