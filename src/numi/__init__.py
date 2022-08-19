8c7b41be4897075124f320d4771f676ed0d54670
DavidErikMollberg <david.mollberg@tiro.is>
2022-08-19 14:21:36 +0000

    standerdizing the output

--- src/numi/__init__.py
+++ src/numi/__init__.py
@@ -46,7 +46,7 @@ from numi.main import spell_out
 # print(spell_out(11451))
 # print(spell_out(21123))
 
-# print(spell_out(151123))
+# 7 print(spell_out(151123))
 
 # print(spell_out(100000))
 # print(spell_out(512321))
@@ -70,3 +70,19 @@ from numi.main import spell_out
 # print(line)
 # for line in spell_out(221123, "ft_hk_þgf"):
 # print(line)
+
+
+# print(spell_out("1,1"))
+# print(spell_out("2,11"))
+# print(spell_out("1,11", "et_kvk_þgf"))
+# print(spell_out("1,111", "et_kvk_þgf"))
+# print(spell_out("1,1111", "et_kvk_þgf"))
+
+
+# print(spell_out(975))
+# print(spell_out(976))
+# print(spell_out(977))
+# print(spell_out(978))
+# print(spell_out(979))
+# print(spell_out(980))
+# print(spell_out(92, "ft_kvk_þgf"))
